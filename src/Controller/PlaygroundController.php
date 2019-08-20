27c394954da932505ff8d3e952d9eba37ab6f236
zemac <etienne.ziegelmeyer@lapiscine.pro>
2019-08-20 17:45:49 +0200

    start code  home page

--- src/Controller/PlaygroundController.php
+++ src/Controller/PlaygroundController.php
@@ -3,10 +3,27 @@
 
 namespace App\Controller;
 
-
+use App\Repository\ParcRepository;
+use App\Repository\PicturesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class PlaygroundController extends AbstractController
 {
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(ParcRepository $parcRepository, PicturesRepository $picturesRepository){
 
+        $parcs = $parcRepository->findAll();
+        $pictures = $picturesRepository->findAll();
+        return $this->render('index.html.twig',
+            [
+            'parcs' => $parcs,
+            'images'=>$pictures
+        ]
+        );
+    }
 }
