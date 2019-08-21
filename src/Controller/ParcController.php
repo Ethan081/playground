4af2428854e41d829942d9711305b5122f82e0ed
zemac <etienne.ziegelmeyer@lapiscine.pro>
2019-08-21 14:27:08 +0200

    add parc controller

--- src/Controller/ParcController.php
+++ src/Controller/ParcController.php
@@ -4,7 +4,24 @@
 namespace App\Controller;
 
 
-class ParcController
+use App\Repository\ParcRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ParcController extends AbstractController
 {
+    /**
+     * @Route("/parc/show/{id}", name="parcshow")
+     */
+    public function parcShow(ParcRepository $parcRepository, $id)
+    {
+        $parcs = $parcRepository->find($id);
+
+        //dump($book);die;
+        return $this->render('parcShow.html.twig',
+            [
+                'parcs'=>$parcs
+            ]);
+
+    }
 
 }
